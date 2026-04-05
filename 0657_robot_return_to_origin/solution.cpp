d2bd921fa85222609e2eed05c1a243315975ea1b
Avnish <avnishpanwar018@gmail.com>
2026-04-05 21:46:12 +0530

    Adding leetcode 0657 cpp solution

---
+++ 0657_robot_return_to_origin/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0,y=0;
+        for(char c:moves){
+            if(c=='R')  x++;
+            else if(c=='L') x--;
+            else if(c=='U') y++;
+            else if(c=='D') y--;
+        }
+        return (x==0 && y==0);
+    }
+};
