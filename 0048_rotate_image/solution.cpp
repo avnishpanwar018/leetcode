de49859513bed6465e0210709d7fba11a5d4e16c
Avnish <avnishpanwar018@gmail.com>
2026-05-04 20:18:11 +0530

    Adding leetcode 0048 cpp solution

---
+++ 0048_rotate_image/solution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n ^ 2)
+// Space Complexity: O(1)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                swap(matrix[i][j],matrix[i][n-j-1]);
+            }
+        }
+        return;
+    }
+};
