a6345c87d2234470e6c2ac6e076fbe0c24d5398f
Avnish <avnishpanwar018@gmail.com>
2026-05-05 20:11:40 +0530

    Adding leetcode 0061 cpp solution

---
+++ 0061_rotate_list/solution.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k==0)    return head;
+        int n=1;
+        ListNode*tail=head;
+        while(tail->next){
+            tail=tail->next;
+            n++;
+        }
+        tail->next=head;
+        k%=n;
+        ListNode*newTail=head;
+        for(int i=1;i<n-k;i++){
+            newTail=newTail->next;
+        }
+        ListNode*newHead=newTail->next;
+        newTail->next=NULL;
+        return newHead;
+    }
+};
