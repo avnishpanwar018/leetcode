d21c2e61a5241fecd676589cad0d25b484aeeaca
Avnish <avnishpanwar018@gmail.com>
2026-05-07 22:36:39 +0530

    Adding leetcode 3660 cpp solution

---
+++ 3660_jump_game_ix/solution.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>maxPrefix(n);
+        vector<int>minSuffix(n);
+        maxPrefix[0]=nums[0];
+        minSuffix[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            maxPrefix[i]=max(maxPrefix[i-1],nums[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            minSuffix[i]=min(minSuffix[i+1],nums[i]);
+        }
+        vector<int>ans(n);
+        ans[n-1]=maxPrefix[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(maxPrefix[i]>minSuffix[i+1])  ans[i]=ans[i+1];
+            else    ans[i]=maxPrefix[i];
+        }
+        return ans;
+    }
+};
