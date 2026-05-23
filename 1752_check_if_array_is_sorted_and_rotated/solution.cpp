69e847edeec9aa544c81b6ef352fb738f48a1113
Avnish <avnishpanwar018@gmail.com>
2026-05-23 22:50:12 +0530

    Adding leetcode 1752 cpp solution

---
+++ 1752_check_if_array_is_sorted_and_rotated/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int cnt=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1])   cnt++;
+        }
+        if(nums[0]<nums[n-1])   cnt++;
+        return cnt<=1;
+    }
+};
