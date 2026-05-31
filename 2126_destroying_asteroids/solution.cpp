0df3f1d7a51bd64e3365083cfe4ea34de6f4a79c
Avnish <avnishpanwar018@gmail.com>
2026-05-31 20:38:28 +0530

    Adding leetcode 2126 cpp solution

---
+++ 2126_destroying_asteroids/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n log n)
+// Space Complexity: O(log n)       // recursion stack from sort()
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        long long m=mass;
+        for( int x:asteroids){
+            if(m>=x)    m+=x;
+            else return false;
+        }
+        return true;
+    }
+};
