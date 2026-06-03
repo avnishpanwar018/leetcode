66916431e65e947f32896a2dae75cebf6dcca6ba
Avnish <avnishpanwar018@gmail.com>
2026-06-03 15:09:51 +0530

    Adding leetcode 3635 cpp solution

---
+++ 3635_earliest_finish_time_for_land_and_water_rides_ii/solution.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(m + n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int landFinish=INT_MAX;
+        int ans=INT_MAX;
+        for(int i=0;i<landStartTime.size();i++){
+            landFinish=min(landFinish,landStartTime[i]+landDuration[i]);
+        }
+        for(int i=0;i<waterStartTime.size();i++){
+            int curr=max(landFinish,waterStartTime[i])+waterDuration[i];
+            ans=min(ans,curr);
+        }
+        int waterFinish=INT_MAX;
+        for(int i=0;i<waterStartTime.size();i++){
+            waterFinish=min(waterFinish,waterStartTime[i]+waterDuration[i]);
+        }
+        for(int i=0;i<landStartTime.size();i++){
+            int curr=max(waterFinish,landStartTime[i])+landDuration[i];
+            ans=min(ans,curr);
+        }
+        return ans;
+    }
+};
