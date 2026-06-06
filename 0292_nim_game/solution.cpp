a98eda8f532ce074b8a415f9452bdf225157f901
Avnish <avnishpanwar018@gmail.com>
2026-06-06 19:36:02 +0530

    Adding leetcode 0292 cpp solution

---
+++ 0292_nim_game/solution.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n%4!=0;
+    }
+};
